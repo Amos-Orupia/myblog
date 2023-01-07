0cbd5556a287ddb17c3f94f57a61129273022764
Amos Orupia <amosorupia712@gmail.com>
2023-01-07 18:40:50 +0300

    first commit

---
+++ App/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+
+# Register your models here.
+from .models import Post
+
+admin.site.register(Post)
+
+#class PostAdmin(admin.ModelAdmin):
+    #list_display = ('title', 'status', 'created_on')
+   # list_filter = ('status',)
+   # search_fields = ['title', 'content']
+
+
